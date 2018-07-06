76252a8e4c1d7897b104d715f11a1e9b8dbc523b
Saiboot <T3eG3e@gmail.com>
2018-07-06 15:13:48 +0200

    Set modification name

--- src/game/server/gamemodes/dm.cpp
+++ src/game/server/gamemodes/dm.cpp
@@ -6,7 +6,7 @@
 CGameControllerDM::CGameControllerDM(class CGameContext *pGameServer)
 : IGameController(pGameServer)
 {
-	m_pGameType = "DM";
+	m_pGameType = "aWorld";
 }
 
 void CGameControllerDM::Tick()
